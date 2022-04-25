835c21e19037d2cd66227996b4986335e391ddbb
Denys Saltan <VI2US@V128S-MAC.local>
2022-04-25 14:58:21 +0300

    Adding local Files

---
+++ src/views/App/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask,
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask,
+    ]);
+
+    console.log(tasks);
+
+
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className={styles.articleSection}>
+                <InputPlus
+                    onAdd={(title) => {
+                        if (title) {
+                            createTask(title);
+                        }
+                    }}
+                />
+            </section>
+            <section className={styles.articleSection}>
+
+            </section>
+        </article>
+    );
+}
---
+++ src/views/components/InputPlus/index.tsx
@@ -0,0 +1,40 @@
+import React, {useState, useCallback} from 'react';
+
+import styles from './index.module.scss';
+
+interface InputPlusProps {
+    onAdd: (title: string) => void;
+}
+
+export const InputPlus: React.FC<InputPlusProps> = ({
+    onAdd,
+}) => {
+    const [inputValue, setInputValue] = useState ('');
+    const addTask = useCallback(() => {
+        onAdd(inputValue);
+        setInputValue('');
+    }, [inputValue])
+    return (
+        <div className={styles.InputPlus}>
+            <input
+                type="text"
+                className={styles.InputPlusValue}
+                value={inputValue}
+                onChange={(evt) => {
+                    setInputValue(evt.target.value);
+                }}
+                onKeyDown={(evt) => {
+                    if (evt.key === 'Enter') {
+                        addTask();
+                    }
+                }}
+                placeholder="Add a task"
+            />
+            <button
+                onClick={addTask}
+                aria-label="Add"
+                className={styles.InputPlusButton}
+            />
+        </div>
+    );
+};
